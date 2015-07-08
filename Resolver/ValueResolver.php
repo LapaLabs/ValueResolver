75c5cbd1b7131d2a9a81ade96c22f2678ef92ddd
Victor <bocharsky.bw@gmail.com>
2015-07-08 22:50:46 +0300

    Add array and object types support

--- Resolver/ValueResolver.php
+++ Resolver/ValueResolver.php
@@ -2,6 +2,8 @@
 
 namespace LapaLabs\ValueResolver\Resolver;
 
+use stdClass;
+
 /**
  * Class ValueResolver
  *
@@ -15,7 +17,11 @@ class ValueResolver
     const TYPE_INT      = 'int';
     const TYPE_INTEGER  = 'integer';
     const TYPE_FLOAT    = 'float';
+    const TYPE_DOUBLE   = 'double';
+    const TYPE_REAL     = 'real';
     const TYPE_STRING   = 'string';
+    const TYPE_ARRAY    = 'array';
+    const TYPE_OBJECT   = 'object';
 
     /**
      * @param mixed $value
@@ -48,11 +54,19 @@ class ValueResolver
                 return (int)static::resolve((int)$value, $default);
 
             case static::TYPE_FLOAT:
+            case static::TYPE_DOUBLE:
+            case static::TYPE_REAL:
                 return (float)static::resolve((float)$value, $default);
 
             case static::TYPE_BOOL:
             case static::TYPE_BOOLEAN:
                 return (bool)static::resolve((bool)$value, $default);
+
+            case static::TYPE_ARRAY:
+                return (array)static::resolve($value, $default);
+
+            case static::TYPE_OBJECT:
+                return (object)static::resolve($value, $default);
 
             default:
                 throw new \InvalidArgumentException(sprintf(
@@ -101,5 +115,29 @@ class ValueResolver
     public static function boolean($value, $default = false)
     {
         return static::typecasting(static::TYPE_BOOLEAN, $value, $default);
+    }
+
+    /**
+     * @param mixed $value
+     * @param array $default = []
+     * @return array
+     */
+    public static function toArray($value, array $default = [])
+    {
+        return static::typecasting(static::TYPE_ARRAY, $value, $default);
+    }
+
+    /**
+     * @param mixed $value
+     * @param object $default = new \stdClass
+     * @return object
+     */
+    public static function toObject($value, stdClass $default = null)
+    {
+        if (null === $default) {
+            $default = new stdClass();
+        }
+
+        return static::typecasting(static::TYPE_OBJECT, $value, $default);
     }
 }
